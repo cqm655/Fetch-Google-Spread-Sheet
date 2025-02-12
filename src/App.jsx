dff46d586fd860253b208e14649e364afbc5815a
iurie.balan <iurie.balan@112.md>
2025-02-12 08:58:47 +0200

    search by name, or by choosen column

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import {useEffect, useState} from 'react'
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import {
+    Box,
+    Input,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import React from 'react';
 
 
 const SPREADSHEET_ID =  "1AN7cQZHWlfGCZhvCpnfDo6yDANaVHAJ8S7HbqmefJpg";
@@ -13,6 +25,8 @@ const noImage = 'https://static.vecteezy.com/system/resources/thumbnails/022/059
 
 function App() {
     const [sheetsData, setSheetsData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [data, setData] = useState(sheetsData);
 
     useEffect(() => {
         const fetchAllSheetsData = async () => {
@@ -46,43 +60,100 @@ function App() {
 
         fetchAllSheetsData();
     }, []);
-    console.log(sheetsData)
+
+    //search data
+    const filteredData = sheetsData.map((sheet) => ({
+        ...sheet,
+        values: sheet.values.filter(
+            (row, index) =>
+                index === 0 || // Păstrăm header-ul
+                (row[2] &&
+                    row[2].toLowerCase().includes(searchTerm.toLowerCase())) // Căutăm în prima coloană (nume)
+        ),
+    }));
+
+    console.log(filteredData)
   return (
-      <Paper sx={{ width: '100%', borderRadius: '21px', backgroundColor:'gray', position: 'relative', paddingBottom: '20px' }}>
-          <Typography variant="h4" sx={{ mb: 2,     marginLeft: '40%' }}>Google Sheets Data</Typography>
-          {sheetsData.map((sheet, sheetIndex) => (
-              <TableContainer component={Paper} sx={{ width: '97%', ml:3, borderRadius: '20px' }} key={sheetIndex}>
-                  <Typography variant="h6" sx={{ p: 2, marginLeft: '45%' }}>Foaie: {sheet.sheetName}</Typography>
+      <Paper
+          sx={{
+              width: "100%",
+              borderRadius: "21px",
+              backgroundColor: "gray",
+              position: "relative",
+              paddingBottom: "20px",
+          }}
+      >
+          <Typography variant="h4" sx={{ mb: 2, marginLeft: "40%" }}>
+              Google Sheets Data
+          </Typography>
+          <Box
+              sx={{
+                  marginBottom: "20px",
+                  ml: "40px",
+                  backgroundColor: "#fff",
+                  borderRadius: "10px",
+                  width: "200px",
+                  height: "40px",
+              }}
+          >
+              <Input
+                  sx={{ ml: 3, mr: 3 }}
+                  placeholder={"Căutare..."}
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+              />
+          </Box>
+          {filteredData.map((sheet, sheetIndex) => (
+              <TableContainer
+                  component={Paper}
+                  sx={{ width: "97%", ml: 3, borderRadius: "20px" }}
+                  key={sheetIndex}
+              >
+                  <Typography variant="h6" sx={{ p: 2, marginLeft: "45%" }}>
+                      Foaie: {sheet.sheetName}
+                  </Typography>
                   <Table>
                       <TableHead>
                           <TableRow>
-                              {sheet.headers.length > 0 && sheet.headers.map((header, colIndex) => (
-                                  <TableCell key={colIndex} sx={{ fontWeight: 'bold', backgroundColor: '#f5f5f5' }}>
-                                      {colIndex === 1 ? "Image" : header || `Column ${colIndex + 1}`}
-                                  </TableCell>
-                              ))}
+                              {sheet.headers.length > 0 &&
+                                  sheet.headers.map((header, colIndex) => (
+                                      <TableCell
+                                          key={colIndex}
+                                          sx={{ fontWeight: "bold", backgroundColor: "#f5f5f5" }}
+                                      >
+                                          {colIndex === 1 ? "Image" : header || `Column ${colIndex + 1}`}
+                                      </TableCell>
+                                  ))}
                           </TableRow>
                       </TableHead>
                       <TableBody>
-                          {sheet.values.slice(1).map((row, rowIndex) => (
-                              <TableRow key={rowIndex}>
-                                  {row.map((cell, colIndex) => (
-                                      <TableCell key={colIndex}>
-                                          {colIndex === 1 && typeof cell === "string" ? (
-                                              <img
-                                                  src={cell.length > 0 ? cell : noImage}
-                                                  alt="No image"
-                                                  width="100"
-                                                  loading="lazy"
-                                                  style={{ borderRadius: 8 }}
-                                              />
-                                          ) : (
-                                              cell || "N/A"
-                                          )}
-                                      </TableCell>
-                                  ))}
+                          {sheet.values.length > 1 ? (
+                              sheet.values.slice(1).map((row, rowIndex) => (
+                                  <TableRow key={rowIndex}>
+                                      {row.map((cell, colIndex) => (
+                                          <TableCell key={colIndex}>
+                                              {colIndex === 1 && typeof cell === "string" ? (
+                                                  <img
+                                                      src={cell.length > 0 ? cell : noImage}
+                                                      alt="No image"
+                                                      width="100"
+                                                      loading="lazy"
+                                                      style={{ borderRadius: 8 }}
+                                                  />
+                                              ) : (
+                                                  cell || "N/A"
+                                              )}
+                                          </TableCell>
+                                      ))}
+                                  </TableRow>
+                              ))
+                          ) : (
+                              <TableRow>
+                                  <TableCell colSpan={sheet.headers.length} sx={{ textAlign: "center", color: "red" }}>
+                                      Nicio potrivire găsită!
+                                  </TableCell>
                               </TableRow>
-                          ))}
+                          )}
                       </TableBody>
                   </Table>
               </TableContainer>
